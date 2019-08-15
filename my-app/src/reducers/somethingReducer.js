923703a46c340109b3bbf7d76e3bafebfa364821
laura <lauradondiego@gmail.com>
2019-08-15 12:57:19 -0400

    added all needed dependencies

---
+++ my-app/src/reducers/somethingReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  webcams: []
+  // or whatever makes sense for the api you choose
+  // see if it is an array or whatever
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
